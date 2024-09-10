2b4ac28e4bdfad46dd963df9ad83d1f59b0b4743
Fellipe-Ortega <felipe.osouza@sptech.school>
2024-09-10 11:30:15 -0300

    Alterei clientes diários para mensaigit add .

--- Banco de dados/bd_spacesense_felipe_miguel.sql
+++ Banco de dados/bd_spacesense_felipe_miguel.sql
@@ -16,13 +16,16 @@ CREATE TABLE sensor (
 	id int primary key auto_increment,
     distancia float, -- DISTANCIA QUE O SENSOR ESTÁ MEDINDO
     data_hora date, -- DATA E HORA QUE HOUVE ALGUM TIPO DE RETIRADA
-    posicao_item varchar(10) -- prateleira que está observado (AJ4)
+    posicao_item varchar(10), -- prateleira que está observado (AJ4)
+	situacao char(1), -- se o sensor está ativo ou não
+    CONSTRAINT chksituacao CHECK (situacao IN (0, 1))
 );
 
 CREATE TABLE posicao_item (
 	id int primary key auto_increment,
 	corredor varchar(10), -- Corredor em que a prateleira está
-    nível int, -- Nível em que a prateleira está
+    nivel int, -- Nível em que a prateleira está
     fileira int, -- Qual a fileira dentro da prateleira o item está
-    qtd_de_acessos varchar (200) -- Qtd de acessos dessa posição
+    qtd_de_acessos varchar (200), -- Qtd de acessos dessa posição 
+    CONSTRAINT chkNivelLocacao CHECK (nivel between 1 and 5)
 );
